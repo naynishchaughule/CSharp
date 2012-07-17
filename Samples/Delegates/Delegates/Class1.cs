e68a6c2b3a868b1617bedd2290977b0737b8330e
naynish <naynish@hotmail.com>
2012-07-17 16:04:02 -0400

    Progress Bar and Delegates

---
+++ Samples/Delegates/Delegates/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Delegates
+{
+    class Class1
+    {        
+        public bool Method1()
+        {
+            int i = 20;
+            if (i < 40)
+            {
+                //successful execution of method
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Method2()
+        {
+            int i = 30;
+            if (i < 40)
+            {
+                //successful execution of method
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Samples/Delegates/Delegates/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Delegates
+{
+    public partial class Form1 : Form
+    {
+        Class1 c1 = new Class1();
+        public delegate bool myCustom();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Methods Executed Successfully:\n";
+            myCustom myDelegateInstance = new myCustom(c1.Method1);
+            myDelegateInstance += c1.Method2;            
+            foreach (Delegate item in myDelegateInstance.GetInvocationList())
+	        {                
+                if(((myCustom)item).Invoke())
+                {
+                    //MessageBox.Show(item.Method.ToString());
+                    progressBar1.Maximum = 20;
+                    progressBar1.Value += 1;
+                    label1.Text += "\n" + item.Method.Name + "\n";
+                }
+	        }             
+        }
+    }
+}
